58c92141d949975e3036dc976862e531553a3de1
Artyom Kurpas <minicoal@gmail.com>
2023-04-14 20:51:00 +1000

    Add structure User

--- user.go
+++ user.go
@@ -1,1 +1,20 @@
 package user
+
+type User struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	//Age            int       `json:"age"`
+	//Mail           string    `json:"mail"`
+	//StatusUser     string    `json:"statusUser"`
+	//EducationLevel string `json:"educationLevel"`
+	//StudyProgramId int       `json:"studyProgramId"`
+	//SchoolId       int       `json:"schoolId"`
+	//AdmissionYear  string    `json:"admissionYear"`
+	//GraduationYear string    `json:"graduationYear"`
+	//IsAdmin        bool      `json:"isAdmin"`
+	//IsVerificated  bool      `json:"isVerificated"`
+	//IsVisible      bool      `json:"isVisible"`
+	//AvatarPath     string    `json:"avatar_path"`
+	//IsModerated    bool      `json:"is_moderated"`
+}
