856289a1d937d7f076c173187da8b66d652c74d6
Matvey <ezepiz@mail.ru>
2023-04-27 19:11:47 +1000

    add private api

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -19,16 +19,14 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	router.POST("/sign-up", h.signUp)
 	router.POST("/sign-in", h.signIn)
-	//router.POST("/test", h.test)
+	router.POST("/test", h.test)
 
-	api := router.Group("/api")
+	apiPublic := router.Group("/api")
 	{
-		users := api.Group("/users")
+		users := apiPublic.Group("/users")
 		{
-			users.POST("/", h.createUser)
 			users.GET("/", h.getAllUsers)
 			users.GET("/:id", h.getUserById)
-			users.DELETE("/:id", h.deleteUser)
 			users.PUT("/:id", h.updateUser)
 
 			activation := users.Group("/activation")
@@ -37,11 +35,21 @@ func (h *Handler) InitRoutes() *gin.Engine {
 			}
 		}
 
-		//api.POST("/mail", h.sendMessageMail)
-
-		faculty := api.Group("/faculty")
+		faculty := apiPublic.Group("/faculty")
 		{
 			faculty.GET("/", h.getAllFaculties)
+		}
+	}
+
+	apiPrivate := router.Group("/apiP")
+	{
+		users := apiPrivate.Group("/users")
+		{
+			users.POST("/", h.createUser)
+			users.GET("/", h.getAllUsers)
+			users.GET("/:id", h.getUserById)
+			users.DELETE("/:id", h.deleteUser)
+			users.PUT("/:id", h.updateUser)
 		}
 	}
 
